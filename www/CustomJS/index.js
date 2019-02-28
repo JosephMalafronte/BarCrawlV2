8699a466759c1c9f951514c8ccde9c808b3439df
Unknown <jgmalafronte@gmail.com>
2019-02-28 14:34:10 -0600

    Change 3

--- www/CustomJS/index.js
+++ www/CustomJS/index.js
@@ -1,9 +1,22 @@
 
+$.getScript('../Models/barCard.js');
+
+
+var baseUrl = "";
+var barCard1;
 
 $(document).ready(function(){
     console.log("Test");
     var appName = "BarCrawl V2";
-    $("#appName").text(appName);
+
+    var getUrl = window.location;
+    baseUrl = getUrl .protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
+
+    barCard1 = new barCard;
+    barCard1.barName = "Galletes";
  });
 
+$("#barCard").click(function (){
+    window.location.replace("/shop.html");
+});
 
---
+++ www/Models/barCard.js
@@ -0,0 +1,15 @@
+
+
+class barCard {
+
+    barName = "";
+    barPictureUrl = "";
+    highlight1 = "";
+    highlight2 = "";
+    highlight1Icon = "";
+    highlight2Icon = "";
+
+    constructor(barName){
+        this.barName = barName;
+    }
+}
