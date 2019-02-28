e835bf0876757f15218b426bd98e6b79d18e52cb
Unknown <jgmalafronte@gmail.com>
2019-02-28 15:58:32 -0600

    Change 4

--- www/CustomJS/index.js
+++ www/CustomJS/index.js
@@ -1,22 +1,63 @@
 
-$.getScript('../Models/barCard.js');
 
+import barCard from '../Models/barCard.js';
+
+var database = firebase.database();
 
 var baseUrl = "";
-var barCard1;
 
 $(document).ready(function(){
-    console.log("Test");
     var appName = "BarCrawl V2";
 
     var getUrl = window.location;
     baseUrl = getUrl .protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
 
-    barCard1 = new barCard;
-    barCard1.barName = "Galletes";
+
  });
 
 $("#barCard").click(function (){
     window.location.replace("/shop.html");
 });
+
+function AppViewModel () {
+    var self = this;
+
+    var barCards = [];
+
+    var dayOfTheWeek = "Wednesday";
+
+    //Database Get Bar Cards
+    var rootRef = firebase.database().ref();
+    var urlRef = rootRef.child("barCards/" + dayOfTheWeek);
+    urlRef.once("value", function(snapshot) {
+      snapshot.forEach(function(child) {
+        var data = child.val();
+        var barCardInsert = new barCard(data.barName);
+        barCardInsert.barPictureUrl = data.barPictureUrl;
+        barCardInsert.highlight1 = data.highlight1;
+        self.barCards.push(barCardInsert);
+      });
+    });
+
+    
+
+    var barCardsOA = [];
+
+    barCards.forEach(function(element) {
+        console.log(element.barPictureUrl);
+        var insert = {
+            barName: element.barName,
+            barPictureUrl: element.barPictureUrl,
+            highlight1: element.highlight1
+        }
+        barCardsOA.push(insert);
+    });
+
+ 
+    self.barCards = ko.observableArray(barCardsOA);
+ 
+}
+
+ko.applyBindings(new AppViewModel());
+
 
